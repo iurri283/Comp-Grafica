3735c7ce06ffc1884dfb9679e230199f79270cb9
iuri <iuriwerneck10@gmail.com>
2023-10-31 13:23:10 -0300

    velocímetro ok

--- trab.js
+++ trab.js
@@ -10,24 +10,32 @@ import {
   InfoBox,
   SecondaryBox,
   BoxSuperior,
+  Velocimetro,
   onWindowResize,
   createGroundPlaneXZ,
 } from "./libs/util/util.js";
 
 import { Carro } from "./carro.js";
 import { Pista } from "./pista.js";
-let scene, renderer, camera, camera2, camTerceiraPessoa, orbit, ambientLight, dirLight; // Initial variables
+let scene,
+  renderer,
+  camera,
+  camera2,
+  camTerceiraPessoa,
+  orbit,
+  ambientLight,
+  dirLight; // Initial variables
 let width = window.innerWidth;
 let height = window.innerHeight;
 scene = new THREE.Scene(); // Create main scene
 //renderer = initRenderer(); // Init a basic renderer
 renderer = new THREE.WebGLRenderer();
-document.getElementById("webgl-output").appendChild( renderer.domElement );  
+document.getElementById("webgl-output").appendChild(renderer.domElement);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type  = THREE.PCFSoftShadowMap;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 ambientLight = new THREE.AmbientLight("rgb(160,160,160)");
-scene.add( ambientLight );
+scene.add(ambientLight);
 camera = new THREE.PerspectiveCamera(
   30,
   window.innerWidth / window.innerHeight,
@@ -38,17 +46,16 @@ dirLight = new THREE.DirectionalLight("rgb(255,255,255)");
 dirLight.castShadow = true;
 dirLight.shadow.mapSize.width = 256;
 dirLight.shadow.mapSize.height = 256;
-dirLight.shadow.camera.near = .1;
+dirLight.shadow.camera.near = 0.1;
 dirLight.shadow.camera.far = 1000;
 dirLight.shadow.camera.left = -50;
 dirLight.shadow.camera.right = 50;
 dirLight.shadow.camera.bottom = -50;
 dirLight.shadow.camera.top = 50;
-dirLight.shadow.bias = -0.0005;  
+dirLight.shadow.bias = -0.0005;
 dirLight.shadow.radius = 20;
 
 scene.add(dirLight);
-
 
 let camPosition = new THREE.Vector3(200, 250, 200);
 let upVec = new THREE.Vector3(0.0, 0.0, 0.0);
@@ -99,17 +106,28 @@ scene.add(plane);
 
 //#############################################################################################
 const carro = new Carro(scene, camera);
+
+//#########################################  VELOCÍMETRO  #####################################
+const velocimetro = new Velocimetro("Velocímetro");
+var velocidadeAtual = carro.moveDistance;
+var velocidadeAnterior = 0;
+var escalaAtual = 1;
+//#########################################  VELOCÍMETRO  #####################################
+
 const pista = new Pista(scene);
 var inicio = 0;
 var auxCam = 0;
 pista.pista1();
-let lookAtVec = new THREE.Vector3(carro.esqueletoCarro.position.x, carro.esqueletoCarro.position.y, carro.esqueletoCarro.position.z);
+let lookAtVec = new THREE.Vector3(
+  carro.esqueletoCarro.position.x,
+  carro.esqueletoCarro.position.y,
+  carro.esqueletoCarro.position.z
+);
 // virtualCamera.lookAt(lookAtVec);
 
 render();
 
-function reiniciaTempo()
-{
+function reiniciaTempo() {
   tVolta.stop();
   tTotal.stop();
   tVolta.elapsedTime = 0;
@@ -131,7 +149,7 @@ function showInformation() {
 
 function alternarPista() {
   keyboard.update();
-  if(auxCam != 2){
+  if (auxCam != 2) {
     if (keyboard.pressed("1")) {
       reiniciaTempo();
       virtualCamera.position.set(200, 250, 200);
@@ -156,14 +174,12 @@ function alternarPista() {
       carro.reset([500, 4, 700], [0, 3.1416, 0]);
     }
   }
-
 }
 
 let spotLight = new THREE.SpotLight(0xffffff);
 //spotLight.map = new THREE.TextureLoader().load(url);
 
 function alternarCamera() {
-
   if (auxCam == 0) {
     auxCam = 1;
   } else if (auxCam == 1) {
@@ -226,15 +242,48 @@ function gameplay() {
     }
   }
 }
+
+function handleClasses() {
+  velocidadeAtual = parseInt((carro.moveDistance * 100).toFixed(2), 10);
+  velocidadeAtual = Math.ceil(velocidadeAtual / 10) * 10;
+  velocidadeAtual = Math.abs(velocidadeAtual);
+
+  let novaClasse = "velocidade-" + velocidadeAtual;
+  let classeAnterior = "velocidade-" + velocidadeAnterior;
+  let el = document.getElementsByClassName("seta-wrapper")[0];
+  if (el.classList.contains(classeAnterior)) {
+    el.classList.remove(classeAnterior);
+    el.classList.add(novaClasse);
+  }
+  velocidadeAnterior = velocidadeAtual;
+}
+
 function controlledRender() {
-  if(pista.numeroPista == 3 || pista.numeroPista == 4) virtualCamera.position.set(carro.esqueletoCarro.position.x, 250, carro.esqueletoCarro.position.z);
+  if (pista.numeroPista == 3 || pista.numeroPista == 4)
+    virtualCamera.position.set(
+      carro.esqueletoCarro.position.x,
+      250,
+      carro.esqueletoCarro.position.z
+    );
   if (auxCam == 0) {
-    if(pista.numeroPista == 1 || pista.numeroPista == 2)
-      camera.position.set(carro.esqueletoCarro.position.x - 100,50,carro.esqueletoCarro.position.z - 80);
-    else if(pista.numeroPista == 3)
-      camera.position.set(carro.esqueletoCarro.position.x - 100,50,carro.esqueletoCarro.position.z + 80);
-    else if(pista.numeroPista == 4)
-      camera.position.set(carro.esqueletoCarro.position.x + 100,50,carro.esqueletoCarro.position.z + 80);
+    if (pista.numeroPista == 1 || pista.numeroPista == 2)
+      camera.position.set(
+        carro.esqueletoCarro.position.x - 100,
+        50,
+        carro.esqueletoCarro.position.z - 80
+      );
+    else if (pista.numeroPista == 3)
+      camera.position.set(
+        carro.esqueletoCarro.position.x - 100,
+        50,
+        carro.esqueletoCarro.position.z + 80
+      );
+    else if (pista.numeroPista == 4)
+      camera.position.set(
+        carro.esqueletoCarro.position.x + 100,
+        50,
+        carro.esqueletoCarro.position.z + 80
+      );
     dirLight.target = carro.esqueletoCarro;
     // camera.position.copy(carro.esqueletoCarro.position+obj.position);
     camera.lookAt(carro.esqueletoCarro.position);
@@ -243,9 +292,29 @@ function controlledRender() {
     renderer.setClearColor("rgb(120, 190, 220)");
     renderer.clear(); // Clean the window
     renderer.render(scene, camera);
-    dirLight.position.set(camera.position.x, camera.position.y+1, camera.position.z-40);
+    dirLight.position.set(
+      camera.position.x,
+      camera.position.y + 1,
+      camera.position.z - 40
+    );
 
     gameplay();
+    // #############################################################  VELOCÍMETRO  #####################################################
+    if (carro.moveDistance * 100 < 180) {
+      if (carro.moveDistance <= 0) {
+        velocidadeAtual = Math.abs(carro.moveDistance);
+      }
+      handleClasses();
+    }
+
+    // if (carro.moveDistance * 100 < 0) {
+    //   carro.moveDistance = 0;
+    // }
+
+    velocimetro.changeMessage(
+      "" + (parseInt((carro.moveDistance * 100).toFixed(2), 10) + "km/h")
+    );
+
     message.changeMessage(
       "Tempo da volta: " +
         tVolta.getElapsedTime().toFixed(2) +
@@ -261,7 +330,11 @@ function controlledRender() {
     // camera.position.copy(carro.esqueletoCarro.position+obj.position);
     camTerceiraPessoa.lookAt(carro.esqueletoCarro.position);
     // requestAnimationFrame(render); // Show events
-    dirLight.position.set(carro.esqueletoCarro.position.x -90, carro.esqueletoCarro.position.y+21, carro.esqueletoCarro.position.z);
+    dirLight.position.set(
+      carro.esqueletoCarro.position.x - 90,
+      carro.esqueletoCarro.position.y + 21,
+      carro.esqueletoCarro.position.z
+    );
     renderer.setViewport(0, 0, width, height); // Reset viewport
     renderer.setScissorTest(false); // Disable scissor to paint the entire window
     renderer.setClearColor("rgb(120, 190, 220)");
@@ -269,6 +342,15 @@ function controlledRender() {
     renderer.render(scene, camTerceiraPessoa);
 
     gameplay();
+
+    if (carro.moveDistance * 100 < 0) {
+      carro.moveDistance = 0;
+    }
+
+    velocimetro.changeMessage(
+      "" + ((carro.moveDistance * 10).toFixed(2) * 10).toFixed(2) + "km/h"
+    );
+
     message.changeMessage(
       "Tempo da volta: " +
         tVolta.getElapsedTime().toFixed(2) +
@@ -300,7 +382,6 @@ function controlledRender() {
 
   // Set main viewport
 
-
   // If autoClear if false, clear depth buffer to avoid unwanted overlays
   if (!renderer.autoClear) renderer.clearDepth(); // Clean the small viewport
 
@@ -317,7 +398,7 @@ function controlledRender() {
   ); // Set scissor with the same size as the viewport - 1
   renderer.setScissorTest(true); // Enable scissor to paint only the scissor are (i.e., the small viewport)
   renderer.setClearColor("rgb(60, 50, 150)"); // Use a darker clear color in the small viewport
-  if (auxCam !=2) renderer.render(scene, virtualCamera); // Render scene of the virtual camera
+  if (auxCam != 2) renderer.render(scene, virtualCamera); // Render scene of the virtual camera
 }
 
 function render() {
